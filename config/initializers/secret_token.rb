054d2aa0be316a223dc4d5a0c206c3c254533132
PhillipKregg <phillipkregg@gmail.com>
2011-10-13 14:19:46 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProTemplateApp::Application.config.secret_token = 'a2cdb7d77c71b64eb10fdb71a1177ba972d91ab1b1768c6bc43ac0a3d5abe5ee287bc996b59fa1a3e185bee341a79aa9b6be1302d35a407b533a5cc2bc69ed86'
